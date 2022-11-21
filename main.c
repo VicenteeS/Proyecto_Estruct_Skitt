a6671f21e1716cb805a87170ad97785dc653bd6d
VicenteeS <vicente.montiel.t@mail.pucv.cl>
2022-11-21 15:04:55 -0300

    Estructuras creadas

--- main.c
+++ main.c
@@ -1,6 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <ctype.h>
+#include "hashmap.h"
+#include "list.h"
+#include "treemap.h"
 
-int main(void) {
-  printf("Hello World\n");
+
+
+typedef struct{
+  char nombre[50];
+  char edad[50];
+  char altura[50]; 
+  char peso[50];
+  char nivel_exp[50];
+  TreeMap *Rutina_fav; 
+}Persona;
+
+typedef struct{
+  char nombre[50];
+  char G_mus [50];
+  char espacio [50];
+  char tiempo[50];
+  char nivel_exp[50];
+  List *L_ejercicios;
+}Rutina;
+
+typedef struct{
+  char nombre[50];
+  char link[100];
+  char G_mus[50];
+  char nivel_exp[50];
+}Ejercicio;
+
+int main()
+{
+  
+  
   return 0;
 }
+
